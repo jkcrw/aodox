f2f3727a36a1321c240eb23df63d0be3c610777c
Jak Crow <jak@jxcrw.io>
2023-03-19 21:31:34 -0700

    ~util: Update pathing in copy script

--- _firmware/copy.py
+++ _firmware/copy.py
@@ -7,7 +7,7 @@ import shutil
 from pathlib import Path
 
 source = Path(__file__).parents[0]
-dest = source.parents[0] / 'qmk_firmware/keyboards/aodox'
+dest = source.parents[1] / 'qmk/keyboards/aodox'
 shutil.copytree(source, dest, dirs_exist_ok=True)
 
 print('\ncopying firmware...done\n')
